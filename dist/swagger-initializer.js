6992e127f8cf801e1d84cf5fe07a18ad857c96b2
gobi <gobidesert.mf@gmail.com>
2024-06-27 14:59:06 +0800

    fix

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "s_voice-library_db-api.json",
+    url: "s_voice-library_db-api.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
